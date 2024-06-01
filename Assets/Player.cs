4bd837cf147cbbb70d462e7c1982d3038ad1246f
WadaKeita <k023g0069@g.neec.ac.jp>
2024-06-01 17:47:18 +0900

    一時保存用 プレイヤーscriptを作成

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region var-Player
+    [Header("プレイヤー")]
+    [SerializeField] public static float moveSpeed = 2f;  // 移動速度
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
